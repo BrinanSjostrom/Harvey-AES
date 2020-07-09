8cd0ffb4c064ddc541724c48a581b7433cf0e4b9
Grim Ops <grim_ops@protonmail.com>
2020-07-09 01:03:22 -0500

    Get rid of key generation ability

--- main.c
+++ main.c
@@ -26,7 +26,6 @@ int main(int argc, char *argv[])
     printf("-m or --mode\t\tSets mode: [e]ncrypt, [d]ecrypt\n");
     printf("-f or --file\t\tSets file input: <filepath>\n");
     printf("-k or --key \t\tSets path to key: <filepath> Needs file to hold 128bit(16 byte) sequence\n");
-    printf("-g or --gen \t\tGenerates 128 bit key and stores in key.txt file\n");
     return 0;
   }
   else if(getArgs('h', "--help", argc, argv))
@@ -35,21 +34,6 @@ int main(int argc, char *argv[])
     printf("-m or --mode\t\tSets mode: [e]ncrypt, [d]ecrypt\n");
     printf("-f or --file\t\tSets file input: <filepath>\n");
     printf("-k or --key \t\tSets path to key: <filepath> Needs file to hold 128bit(16 byte) sequence\n");
-    printf("-g or --gen \t\tGenerates 128 bit key and stores in key.txt file\n");
-    return 0;
-  }
-
-  if(getArgs('g', "--gen", argc, argv))  // Generate a 128 bit key (Not useable)
-  {
-    unsigned char genKey[16];
-    srand(time(0));
-    printf("[+] Generating key...\n");
-    for(int i = 0; i < 16; i++)
-    {
-      genKey[i] = rand() % 256;
-    }
-    writeFile("key.txt", 16, &genKey[0]);
-    printf("[+] Generated!\n");
     return 0;
   }
 
